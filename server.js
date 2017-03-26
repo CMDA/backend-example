ccc24fa2375e226402223bbbd9ade96a75cefaae
Titus Wormer <tituswormer@gmail.com>
2017-03-26 14:00:50 +0200

    Add express server

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var request = require('request')
+var host = 'http://dennistel.nl/movies/'
+
+express()
+  .use(express.static('public'))
+  .set('view engine', 'ejs')
+  .set('views', 'view')
+  .get('/', movies)
+  .get('/:id', movie)
+  .listen(2000)
+
+function movies(req, res, next) {
+  request(host, function (err, response, body) {
+    if (err) {
+      next(err);
+    } else {
+      res.render('index.ejs', {movies: JSON.parse(body)});
+      next();
+    }
+  });
+}
+
+function movie(req, res, next) {
+  request(host + req.params.id, function (err, response, body) {
+    if (err) {
+      next(err);
+    } else {
+      res.render('detail.ejs', {movie: JSON.parse(body)});
+      next();
+    }
+  });
+}
